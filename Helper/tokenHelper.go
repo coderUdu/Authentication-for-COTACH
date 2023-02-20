73810d96ec4e15b05f59746992d78f1bdf8cda00
Alex Bekaren <uduvinci@gmail.com>
2023-02-20 23:54:11 +0100

    first commit on token generator

--- Helper/tokenHelper.go
+++ Helper/tokenHelper.go
@@ -1,10 +1,27 @@
 package helper
 
+import (
+	"os"
 
+	"github.com/coderudu/cotach/database"
 
+	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
+type signedDetails struct {
+	Email      string
+	First_Name string
+	Last_Name  string
+	UID        string
+	User_type  string
+	jwt.StandardClaims
+}
 
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, user)
 
-func GenerateAllTokens(){
-	
+var secret_key string =os.Getenv("secretkey")
+
+func GenerateAllTokens() {
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/coderudu/cotach
 go 1.19
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.8.2
 	github.com/go-playground/validator/v10 v10.11.1
 	go.mongodb.org/mongo-driver v1.11.2
