ac0cbb631b94615d916b7aea17b49583aeb07ab6
Alex Bekaren <uduvinci@gmail.com>
2023-02-21 23:45:04 +0100

    get user By ID controller

--- Controllers/userController.go
+++ Controllers/userController.go
@@ -177,7 +177,31 @@ func Login() gin.HandlerFunc {
 
 
 
-func GetUserByID() {
+func GetUserByID() gin.HandlerFunc{
+
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	}
 
 }
 
